540cf15216264de2335efedd9c300008cb52ba71
alvaro <aperez15@student.aau.dk>
2016-03-29 13:52:27 +0200

    main changes

--- src/matlab/main_Cobra.m
+++ src/matlab/main_Cobra.m
@@ -12,7 +12,7 @@
 %
 %**************************************************************************
 
-clc; clear all; close all;
+close all; clear all; clc;
 
 %**************************************************************************
 %
@@ -28,11 +28,57 @@ clc; clear all; close all;
 %
 %**************************************************************************
 
-% Creates the conector object
-RobotConnector;
-r = RobotConnector;
+%% Conection between robot and computer  
+% % Creates the conector object
+% RobotConnector;
+% r = RobotConnector;
+% 
+% %r.moveLinear
+% %r.moveJoint
+% r.closeGrapper
+% r.openGrapper
 
-%r.moveLinear
-%r.moveJoint
-r.closeGrapper
-r.openGrapper
+%% Structure of the code
+
+% 1 - User input the figure he wants to get
+% 2 - Detect blocks by color
+% 3 - Calculate their coordinates based in the edges
+% 4 - Actuate with the robot
+
+%% Global variables
+
+STR_HOMER = 'bwy';      % Bottom to top: Blue, white and yellow
+STR_MARGE = 'gyb';      % Bottom to top: Green, yellow and blue
+STR_BART = 'boy';       % Bottom to top: Blue, orange, yellow
+
+%% USER INTERFACE
+% Program starting
+fprintf('##############################################################################\n');
+fprintf('#                           COBRA SIMPSONS BUILDER                           #\n');
+fprintf('#                                                                            #\n');
+fprintf('#        GROUP 832 - CONTROL AND AUTOMATION MSC. - AALBORG UNIVERSITY        #\n');
+fprintf('##############################################################################\n\n');
+fprintf('Choose one character from The Simpsons series.\n');
+fprintf('Input either the name of the character or its respective letter of the list:\n');
+prompt = 'A) Homer\nB) Marge\nC) Bart\n\nYour answer: ';
+
+usrIn = input(prompt,'s');
+if (strcmp(usrIn,'homer') || strcmp(usrIn,'A') || strcmp(usrIn,'a'))
+    fprintf('\nYou chose: A) Homer\n');
+    build = STR_HOMER;
+elseif (strcmp(usrIn,'marge') || strcmp(usrIn,'B') || strcmp(usrIn,'b'))
+    fprintf('\nYou chose: B) Marge\n');
+    build = STR_MARGE;
+elseif (strcmp(usrIn,'bart') || strcmp(usrIn,'C') || strcmp(usrIn,'c'))
+    fprintf('\nYou chose: C) Bart\n');
+    build = STR_BART;
+else
+    fprintf('\nWARNING: Empty or unrecognized request.\nAssuming figure: A) Homer...\n');
+    usrIn = 'homer';
+end
+
+%% Loading/getting image from camera
+
+test = imread('testBlocks.png');
+
+

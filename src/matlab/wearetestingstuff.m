4ed8b6774de897a65aea5323da5575ce11e4ae05
kpagels <kelvin@pagels.dk>
2016-04-02 14:01:23 +0200

    ..

--- src/matlab/wearetestingstuff.m
+++ src/matlab/wearetestingstuff.m
@@ -1,5 +1,17 @@
 close all; clear all; clc;
 
+% Camera from matlab
+% Camera connection
+
+webcamlist           % shows available cameras on pc
+cam = webcam(2)      % store camera in a variable and shows parameters
+preview(cam)         % camera preview (stream video)
+I = snapshot(cam);   % take a picture
+image(I)             % show image
+%I = imread('testBlocks.png');
+%imtool(I)           % read RGB colors from an image to do thresholding
+
+
 %% New calibration
 
 load('Calib_Results.mat')

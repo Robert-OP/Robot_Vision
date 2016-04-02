3b263f243e82123d655d9250ddee34b2fac5b458
alvaro <aperez15@student.aau.dk>
2016-04-02 16:31:07 +0200

    TRANSLATION

--- src/matlab/wearetestingstuff.m
+++ src/matlab/wearetestingstuff.m
@@ -1,23 +1,23 @@
-close all; clear all; clc;
-
-% Camera from matlab
-% Camera connection
-
-webcamlist           % shows available cameras on pc
-cam = webcam(2)      % store camera in a variable and shows parameters
-preview(cam)         % camera preview (stream video)
-I = snapshot(cam);   % take a picture
-image(I)             % show image
-%I = imread('testBlocks.png');
-%imtool(I)           % read RGB colors from an image to do thresholding
-
-imwrite(I,'fig/chessboard4.jpg')
+% close all; clear all; clc;
+% 
+% % Camera from matlab
+% % Camera connection
+% 
+% webcamlist           % shows available cameras on pc
+% cam = webcam(2)      % store camera in a variable and shows parameters
+% preview(cam)         % camera preview (stream video)
+% I = snapshot(cam);   % take a picture
+% image(I)             % show image
+% %I = imread('testBlocks.png');
+% %imtool(I)           % read RGB colors from an image to do thresholding
+% 
+% imwrite(I,'fig/chessboard4.jpg')
 %% New calibration
 
 load('Calib_Results.mat')
 
-img = rgb2gray(imread('fig/chessboard.jpg'));
-% img = rgb2gray(imread('fig/chessboard1.jpg'));
+% img = rgb2gray(imread('fig/chessboard4.jpg'));
+img = rgb2gray(imread('fig/chessboard1.jpg'));
 
 imshow(img);
 cd('fig/');
